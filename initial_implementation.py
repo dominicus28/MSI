7c182fa51c7d71f48f5b332957690bbc75ae5495
domi28 <dominik.domin740@gmail.com>
2023-04-18 20:20:34 +0200

    Compare AdaBoost with others algorithms

--- initial_implementation.py
+++ initial_implementation.py
@@ -1,15 +1,19 @@
 import numpy as np
 import sklearn
 import csv
+import tabulate
 
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.datasets import make_classification
 from sklearn.model_selection import RepeatedKFold
 from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import BaggingClassifier
+from tabulate import tabulate
 
 class AdaBoostClassifier:
     
-    def __init__(self,n_estimators=50):
+    def __init__(self,n_estimators):
         self.n_estimators = n_estimators
         self.models = []
         self.model_errors = []
@@ -66,23 +70,26 @@ with open(r"C:\Users\Admin\Downloads\wifi_localization_csv.txt") as f:
     for row in reader:
         X = np.append(X, np.array([[int(row[0]), int(row[1]), int(row[2]), int(row[3]), int(row[4]), int(row[5]), int(row[6])]]), axis=0)
         y = np.append(y, int(row[7]))
+        
+rkf = RepeatedKFold(n_repeats=5, n_splits=2, random_state=0)
+score = np.empty((10, 5), float)
 
-#print(X)
-#print(y)
-
-
-rkf = RepeatedKFold(n_repeats=5, n_splits=2, random_state=1)
-score = []
+clf50 = AdaBoostClassifier(n_estimators=50)
+clf100 = AdaBoostClassifier(n_estimators=100)
+RFclf = RandomForestClassifier(max_depth=2, random_state=0)
+GBclf = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=0)
+BGclf = BaggingClassifier(estimator=DecisionTreeClassifier(max_depth=1), n_estimators=10, random_state=0)
+CLFs = [clf50, clf100, RFclf, GBclf, BGclf]
 
 for i, (train_index, test_index) in enumerate(rkf.split(X, y)):
-    clf = AdaBoostClassifier()
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
-    clf.fit(X_train, y_train)
-    prediction = clf.predict(X_test)
-    score.append(accuracy_score(y_test, prediction))
+    for j in range(len(CLFs)):
+        CLFs[j].fit(X_train, y_train)
+        prediction = CLFs[j].predict(X_test)
+        score[i][j] = accuracy_score(y_test, prediction)
 
-print('Mean score: {:.1%}'.format(np.mean(score)))
+np.save('results.npy', score)
+print(tabulate([['Mean score', '{:.1%}'.format(np.mean(score[:,0])), '{:.1%}'.format(np.mean(score[:,1])), '{:.1%}'.format(np.mean(score[:,2])), '{:.1%}'.format(np.mean(score[:,3])), '{:.1%}'.format(np.mean(score[:,4]))], ['Deviation score', '{:.1%}'.format(np.std(score[:,0])), '{:.1%}'.format(np.std(score[:,1])), '{:.1%}'.format(np.std(score[:,2])), '{:.1%}'.format(np.std(score[:,3])), '{:.1%}'.format(np.std(score[:,4]))]], headers=[' ', 'AdaBoost 50xkNN', 'AdaBoost 100xkNN', 'Random Forest', 'GradientBoosting', 'Bagging (with kNN)']))
 
-print('Deviation score: {:.1%}'.format(np.std(score)))
 #print('The scikit-learn version is {}.'.format(sklearn.__version__))
